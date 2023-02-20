412195407aab4a1df3d827dd974e0153f7d2c654
Bilal Hussain <bilalhussian785@gmail.com>
2023-02-20 11:40:39 +0500

    Added users and assosiate with the articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   # validates :title, presence: true, length: { maximum: 255, minimum: 5 }
   # validates :body, presence: true, length: { maximum: 255, minimum: 5 }
   validates :title, presence: true, length: {minimum: 5, maximum: 200}
   validates :description, presence: true, length: {minimum: 5, maximum: 300}
+  validates :user_id, presence: true
 end
